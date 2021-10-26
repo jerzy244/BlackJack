c7cd2335fb737d21f98fc2531f3c6895796d1629
Jerzy <jerzy.n244@gmail.com>
2021-10-26 12:44:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+let cards = [] 
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector(".sum-el")
+let cardsEl = document.querySelector(".cards-el")
+
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) +1
+        if (randomNumber === 1){
+            return 11
+        }else if(randomNumber > 10){
+            return 10
+        } else{
+            return randomNumber
+    }
+}
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+function renderGame(){
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum 
+   
+    if (sum <= 20){
+        message = "Maybe another card?"
+    }else if (sum === 21){
+        message = "Congratulations you won!"
+        hasBlackJack = true
+        // isAlive = false
+    } else {
+        message = "Sadly, you lost :("
+        isAlive = false 
+    }
+    
+    messageEl.textContent = message
+}
+function newCard(){
+    if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
+}
+
